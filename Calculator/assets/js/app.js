e23e086c68fe3d6ea71f3c058df8a6480060f439
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-01-21 16:15:36 +0300

    second commit

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -22,3 +22,38 @@ function div(num1, num2){
     return parseInt(num1) / parseInt(num2);
 }
 
+(function(){
+    var choice = prompt("Enter 1 for addition /nEnter 2 for subtraction /nEnter 3 for multiplication /nEnter 4 for division");
+    if(choice == 1){
+        var myArray = new Array();
+        do{
+            var numbers = prompt("Enter numbers and x to exit:");
+            if(numbers != "x"){
+                myArray.push(parseInt(numbers));
+            }
+        }while(numbers != "x");
+        console.log(add(myArray));
+        
+    }
+    else if(choice == 2){
+        var number1 = prompt("Enter the first number:");
+        var number2 = prompt("Enter the second number:");
+        console.log(sub(number1, number2));
+    }
+    else if(choice == 3){
+        var myArray = new Array();
+        do{
+            var numbers = prompt("Enter numbers and x to exit:");
+            if(numbers != "x"){
+                myArray.push(parseInt(numbers));
+            }
+        }while(numbers != "x");
+        console.log(mult(myArray));
+        
+    }
+    else if(choice == 4){
+        var number1 = prompt("Enter the first number:");
+        var number2 = prompt("Enter the second number:");
+        console.div(add(number1, number2));
+    }
+})();
